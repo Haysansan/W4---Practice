a1f15df6a7faede1f175d03ce8eff4680a787bee
Haysansan <haysan.eang@student.cadt.edu.kh>
2026-02-23 00:20:47 +0700

    TASK-002 – Downloader – Color Theme

--- lib/2_download_app/ui/screens/downloads/widgets/download_tile.dart
+++ lib/2_download_app/ui/screens/downloads/widgets/download_tile.dart
@@ -1,18 +1,55 @@
 import 'package:flutter/material.dart';
- 
+import 'package:w4_practices/2_download_app/ui/providers/theme_color_provider.dart';
+import 'package:w4_practices/2_download_app/ui/theme/theme.dart';
 import 'download_controler.dart';
 
 class DownloadTile extends StatelessWidget {
   const DownloadTile({super.key, required this.controller});
 
   final DownloadController controller;
- 
- // TODO
+
+  // TODO
 
   @override
   Widget build(BuildContext context) {
-    return Placeholder();
-     
     // TODO
+    return ListenableBuilder(
+      listenable: controller,
+      builder: (context, child) {
+        final result = controller.ressource;
+        final currentMB = (controller.progress * result.size).toStringAsFixed(
+          1,
+        );
+
+        return Card(
+          margin: const EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+          child: ListTile(
+            title: Text(result.name, style: AppTextStyles.label),
+            subtitle: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  "${(controller.progress * 100).toInt()}% completed ($currentMB of ${result.size} MB)",
+                ),
+                LinearProgressIndicator(
+                  value: controller.progress,
+                  color: themeProvider.themeColor.color,
+                ),
+              ],
+            ),
+            trailing: IconButton(
+              icon: Icon(
+                controller.status == DownloadStatus.downloaded
+                    ? Icons.check_circle
+                    : Icons.download,
+              ),
+              onPressed: controller.status == DownloadStatus.notDownloaded
+                  ? controller.startDownload
+                  : null,
+            ),
+          ),
+        );
+      },
+    );
   }
 }
